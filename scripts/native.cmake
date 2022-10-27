803a47cd25069b83290afbac44ccfedaace215ef
Ricardo Salveti <ricardo@foundries.io>
2022-10-26 23:48:03 -0300

    Add scripts/native.cmake for compatibility
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

---
+++ scripts/native.cmake
@@ -0,0 +1,21 @@
+# Copyright 2019 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+MACRO(CREATE_BINARY PROJECT_NAME)
+    # Nothing here.
+ENDMACRO()
+
+MACRO(
+    COPY_TO_SOURCEDIR
+    PROJECT_NAME
+    TARGET_DIRNAME
+    TARGET_SUFFIX
+)
+    # Nothing here.
+ENDMACRO()
+
+ADD_DEFINITIONS("-Wno-main-return-type")
+ADD_DEFINITIONS("-Wformat")
+ADD_DEFINITIONS(-DFTR_FILE_SYSTEM)
